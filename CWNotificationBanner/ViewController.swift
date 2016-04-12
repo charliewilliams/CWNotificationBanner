66aaf159db00ea75b05ca408374539fdc7b92662
Charlie Williams <c@charliewilliams.org>
2016-04-12 11:26:57 +0100

    Demo UI

--- CWNotificationBanner/ViewController.swift
+++ CWNotificationBanner/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let message = Message(text: "Hello there", action: {
+            
+        })
+        
+        PopDownOverlayBar.showMessage(message)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var count: Int = 0
+    @IBAction func sendPushPressed(sender: UIButton) {
+        
+        let message = Message(text: "Hello there, this is message \(count)", action: {
+            
+        })
+        
+        PopDownOverlayBar.showMessage(message)
+        
+        count += 1
     }
-
+    
 
 }
 
